fa86b22ad3a2d4cd307ee18de70645bb9b72b9ea
Bill Cox <waywardgeek@google.com>
2016-07-05 04:49:27 -0700

    working on binding

--- core/bind.c
+++ core/bind.c
@@ -1,20 +1,42 @@
 #include "core_int.h"
 
-// Bind identifiers recursively under this ident's scope.
-static void bindListIdentifiers(xyList list, xyIdent ident) {
+// Find any scoped identifier in the list.  It is illegal to have more than
+// one, or to have one in a scoped list since scoped lists are anonymous
+// namespaces.
+static xyToken findScopedIdentToken(xyList list) {
     xyToken token;
+    xyToken scopedToken = xyTokenNull;
     xyForeachListToken(list, token) {
-        coBindIdentifiers(token, ident);
-        if(xyListGetIdent(list) != xyIdentNull) {
-            // This happens when hitting scopped identifiers
-            ident = xyListGetIdent(list);
+        if(xyTokenGetType(token) == XY_IDSCOPED) {
+            if(scopedToken != xyTokenNull) {
+                xyError(token, "List contains two scoped identifiers");
+            }
+            scopedToken = token;
         }
     } xyEndListToken;
-    if(xyListScoped(list) && xyListGetIdent(list) == xyIdentNull) {
-        // This is an unnamed anonymous scope.
-        xyIdent unnamedIdent = xyIdentCreate(ident, utSymNull);
-        xyListSetIdent(list, unnamedIdent);
+    return scopedToken;
+}
+
+// Bind identifiers recursively under this ident's scope.
+static void bindListIdentifiers(xyList list, xyIdent parentScope) {
+    xyToken token = findScopedIdentToken(list);
+    if(token != xyTokenNull) {
+        if(xyListScoped(list)) {
+            xyError(token, "Anonymous scoped list has a scoped identifier");
+        }
+        utAssert(xyTokenGetIdent(token) == xyIdentNull);
+        parentScope = xyIdentCreate(parentScope, xyTokenGetSymVal(token));
+        xyTokenSetIdent(token, parentScope);
+        xyListSetIdent(list, parentScope);
+        xyListSetScoped(list, true);
+    } else if(xyListScoped(list)) {
+        // Create an anonymous scope.
+        parentScope = xyIdentCreate(parentScope, utSymNull);
+        xyListSetIdent(list, parentScope);
     }
+    xyForeachListToken(list, token) {
+        coBindIdentifiers(token, parentScope);
+    } xyEndListToken;
 }
 
 // Bind identifiers recursively under this ident's scope.
@@ -37,14 +59,8 @@ void coBindIdentifiers(xyToken token, xyIdent parentScope) {
         xyTokenSetIdref(token, idref);
     } else if(type == XY_IDDOT) {
         // TODO: Deal with dotted paths when we do type propagation
+        // I don't recall why I had that TODO.  Isn't this just the same as the XY_IDREF case?
     } else if(type == XY_IDSCOPED) {
-        utAssert(xyTokenGetIdent(token) == xyIdentNull);
-        xyIdent ident = xyIdentCreate(parentScope, xyTokenGetSymVal(token));
-        xyTokenSetIdent(token, ident);
-        xyList list = xyTokenGetList(token);
-        utAssert(list != xyListNull);
-        xyListSetScoped(list, true);
-        xyListSetIdent(list, ident);
     } else if(type == XY_LIST) {
         bindListIdentifiers(xyTokenGetListVal(token), parentScope);
     }
